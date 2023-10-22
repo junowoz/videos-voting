baddbeee6b6e16a73c2e126c1adb4fde6efab893
junowoz <junoguvea@gmail.com>
2023-10-22 00:55:18 -0400

    web3 connect wallet button

--- components/main/Navbar.tsx
+++ components/main/Navbar.tsx
@@ -1,11 +1,37 @@
+"use client";
 import React, { useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
+import Web3 from "web3";
+
+declare global {
+  interface Window {
+    ethereum: any;
+  }
+}
+
 
 export const Navbar = () => {
-  const connectWalletHandler = () => {};
+
+  const [web3, setWeb3] = useState<Web3 | null>(null);
+
+const connectWalletHandler = async () => {
+  if (window.ethereum) {
+    try {
+      const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+      const provider = window.ethereum;
+      const web3Instance = new Web3(provider);
+      setWeb3(web3Instance);
+    } catch (error) {
+      console.error("User denied account access");
+    }
+  } else {
+    console.error("Ethereum browser not detected. Consider installing MetaMask.");
+  }
+};
+
 
   return (
     <nav className="bg-slate-200 text-white py-4">
@@ -34,7 +60,9 @@ export const Navbar = () => {
 
         {/* Botão */}
         <div>
-          <Button className="bg-slate-800 hover:bg-slate-600 p-4 rounded text-white">
+          <Button 
+          onClick={connectWalletHandler}
+          className="bg-slate-800 hover:bg-slate-600 p-4 rounded text-white">
           Conectar Carteira
           </Button>
         </div>
